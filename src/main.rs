f6158df7e3a9d868236d6a3029ead1f2904e9194
Robert Habermeier <rphmeier@gmail.com>
2018-12-27 20:35:57 +0100

    get new work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpStream;
 use std::io::{self, Read, Write, BufReader, BufWriter};
 use std::thread;
+use std::sync::mpsc::{self, TryRecvError};
 
 // Supported commands:
 // send pixel: 'PX {x} {y} {GG or RRGGBB or RRGGBBAA as HEX}\n'
@@ -16,6 +17,8 @@ const ADDR: &str = "151.217.40.82:1234";
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
 
+const THREADS: u32 = 20;
+
 fn chunkify_square(x: u32, y: u32, size: u32, rgb: u32, n: u32) -> Vec<Vec<u8>> {
     let lines_per = size / n;
 
@@ -23,7 +26,6 @@ fn chunkify_square(x: u32, y: u32, size: u32, rgb: u32, n: u32) -> Vec<Vec<u8>> 
         let mut v = Vec::new();
 
         let line_off = i*lines_per;
-        println!("Worker {} doing lines {}..{}", i, x + line_off, x + line_off + lines_per - 1);
 
         for line in 0..lines_per {
             for px_y in 0..size {
@@ -36,28 +38,56 @@ fn chunkify_square(x: u32, y: u32, size: u32, rgb: u32, n: u32) -> Vec<Vec<u8>> 
     }).collect()
 }
 
-// run a worker with a programmed batch.
-fn worker(chunk: &[u8]) -> io::Result<()> {
+struct Work(Vec<u8>);
+
+// run a worker with a command receiver.
+fn worker(rx: &mpsc::Receiver<Work>) -> io::Result<()> {
     let mut stream = BufWriter::new(TcpStream::connect(ADDR)?);
 
+    let mut chunk = match rx.recv() {
+        Ok(Work(chunk)) => chunk,
+        Err(e) => panic!("main thread never sent work: {:?}", e),
+    };
+
     loop {
-        stream.write_all(chunk)?;
+        // check for new work.
+        match rx.try_recv() {
+            Ok(Work(new_chunk)) => { chunk = new_chunk },
+            Err(TryRecvError::Empty) => {},
+            Err(_) => panic!("main thread hung up."), 
+        }
+
+        stream.write_all(&chunk[..])?;
         stream.flush()?;
     }
 }
 
+struct Worker {
+    commands: mpsc::Sender<Work>,
+    join_handle: thread::JoinHandle<()>,
+}
+
 fn main() {
-    let mut handles = Vec::new();
-    let chunks = chunkify_square(300, 300, 700, 0xFF0000, 16);
-    for (i, chunk) in chunks.into_iter().enumerate() {
-        handles.push(thread::spawn(move || loop {
-            if let Err(e) = worker(&chunk[..]) {
+    let mut workers = Vec::new();
+    
+    for i in 0..THREADS {
+        let (tx, rx) = mpsc::channel();
+
+        let join_handle = thread::spawn(move || loop {
+            if let Err(e) = worker(&rx) {
                 println!("Restarting thread {}: {:?}", i, e);   
             }
-        }));
+        });
+
+        workers.push(Worker { commands: tx, join_handle });
+    }
+
+    let chunks = chunkify_square(300, 300, 400, 0xFF0000, THREADS);
+    for (i, chunk) in chunks.into_iter().enumerate() {
+        let _ = workers[i].commands.send(Work(chunk));
     }
     
-    for handle in handles {
-        handle.join();
+    for handle in workers {
+        handle.join_handle.join().unwrap();
     }
 }
