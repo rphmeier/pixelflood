4fd5391681a32089151dbeec08cb158458d3e363
Robert Habermeier <rphmeier@gmail.com>
2018-12-27 19:28:42 +0100

    avoid allocation and offset fn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::net::TcpStream;
-use std::io::{Read, Write, BufReader, BufWriter};
+use std::io::{self, Read, Write, BufReader, BufWriter};
 
 // production addr.
 const ADDR: &str = "151.217.40.82:1234";
+
+struct WrappedSlice<'a> {
+    inner: &'a mut [u8],
+    len: usize,
+}
+
+impl<'a> Write for WrappedSlice<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let size = self.inner.write(buf)?;
+        self.len += size;
+        Ok(size)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
+    }
+}
 
 struct Remote<'a> {
     output: BufWriter<&'a TcpStream>,
@@ -24,10 +41,26 @@ impl<'a> Remote<'a> {
         }
     }
 
-    fn set_pixel(&mut self, x: u32, y: u32, rgb: u32) {
-        let out = format!("PX {} {} {:06X}\n", x, y, rgb);
+    fn set_offset(&mut self, x: u32, y: u32) {
+        let mut slice = [0; 32];
+        let len = {
+            let mut wrapped = WrappedSlice { inner: &mut slice, len: 0 };
+            write!(wrapped, "OFFSET {} {}\n", x, y).expect("greater than 32??");
+            wrapped.len
+        };
 
-        self.output.write(out.as_bytes()).expect("could not write");
+        self.output.write(&slice[0..len]).expect("could not write");
+    }
+
+    fn set_pixel(&mut self, x: u32, y: u32, rgb: u32) {
+        let mut slice = [0; 64];
+        let len = {
+            let mut wrapped = WrappedSlice { inner: &mut slice, len: 0 };
+            write!(wrapped, "PX {} {} {:06X}\n", x, y, rgb).expect("greater than 64??");
+            wrapped.len
+        };
+
+        self.output.write(&slice[0..len]).expect("could not write");
     }
 
     fn flush(&mut self) {
@@ -42,9 +75,10 @@ fn main() {
     loop {
         for i in 0..100 {
             for j in 0..100 {
-                remote.set_pixel(i, j, 0x00FF00);
+                remote.set_pixel(off + i, off + j, 0x00FF00);
             }
-            remote.flush();
         }
+
+        remote.flush();
     }
 }
